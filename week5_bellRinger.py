31afe5296a775f6de23f0acaf0e194f5c07ba49c
Joce-Alva <jocealvarez8002@gmail.com>
2025-11-05 20:00:31 +0000

    bellringer

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -1,16 +1,22 @@
 # Problem Set 1: Indexing and Slicing Strings
 # Basic Indexing:
-# Given the string magic = 'abracadabra',
+# Given the string 
+magic = 'abracadabra'
 # a. Retrieve the 5th character.
+print(magic[4])
 # b. Retrieve the second to last character.
+print(magic[9])
 # c. Find the first occurrence of the letter 'c'.
-
+print(magic[4])
 # Advanced Slicing:
-# Given the string alphabet = 'abcdefghijklmnopqrstuvwxyz',
+# Given the string 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # a. Extract the letters 'hij'.
+print(alphabet[7:10])
 # b. Extract every second letter starting from 'a' to 'm'.
+print(alphabet[0][2][4][6][8][10])
 # c. Reverse the entire string using slicing.
-
+print(alphabet[::-1])
 # Problem Set 2: Extracting Information
 # From Descriptions:
 # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
